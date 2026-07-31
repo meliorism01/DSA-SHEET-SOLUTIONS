d44616f8187a357322ec99dd50a97fa9240a2e3b
Manik Mehra <ipsofficermanik@gmail.com>
2026-07-31 22:40:36 +0530

    solved using optimal approach

--- Miscellaneous/LeetCode69. Sqrt(x).cpp
+++ Miscellaneous/LeetCode69. Sqrt(x).cpp
@@ -27,6 +27,7 @@
 // because if a value doesnt have a perfect square root, and has a float square root
 // the square root will be rounded to the floor value
 // and in case of perfect square root, we use the condition i*i==x, returning i;
+// Time Complexity : O(n)
 class Solution {
 public:
     int mySqrt(int x) {
@@ -37,6 +38,33 @@ public:
                     return i;
                 }
         }
+        return 0;
+    }
+};
+
+
+//Optimal Approach:
+// using Binary Search
+// Time complexity : O(logn), but creates a significantly large difference from the bruteforce approach 
+// as the given constraints are 0 <= x <= 231 - 1
+
+class Solution {
+public:
+    int mySqrt(int x) {
+       long start = 0;
+       long end = x;
+       while(start<=end){
+        long mid = (start+end)/2;
+        if((mid*mid)>x&&((mid-1)*(mid-1))<x){
+                    return mid-1;
+                }else if((mid*mid)==x){
+                    return mid;
+                }else if((mid*mid)>x){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+       }
         return 0;
     }
 };
